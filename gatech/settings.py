0ecea6c9b43e31456a9f31d2b2b72a9ccd3d0f26
Celeste Roselli <celesteroselli@Celestes-MacBook-Pro.local>
2023-03-05 17:22:05 -0500

    collected static and added to requirements.txt

--- gatech/settings.py
+++ gatech/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -143,7 +144,3 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None  
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'  
-
-AWS_S3_REGION_NAME = "us-east-2"  
-
-AWS_S3_ADDRESSING_STYLE = "virtual"
